476c4fd1a2023dd4bd091e85d3eb35ca1d3bb1d7
Warren Wiens <wwiens@gmail.com>
2025-11-30 16:56:00 -0600

    Initial commit

---
+++ math.js
@@ -0,0 +1,16 @@
+// math.js
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+// A function to check if a number is even
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+module.exports = { add, subtract, isEven };
